c3dc667ff3a9ff79edaeee9ce53586bbd0c934ac
Ngọc Châu <chaudanhthingoc205@gmail.com>
2026-01-26 20:09:48 +0700

    feat: complete login and register pages

--- src/components/atomic/atoms/Button/Button.tsx
+++ src/components/atomic/atoms/Button/Button.tsx
@@ -2,18 +2,20 @@ type ButtonProps = {
   children: React.ReactNode;
   onClick?: () => void;
   type?: "button" | "submit";
+  className?: string;
 };
 
 export default function Button({
   children,
   onClick,
   type = "button",
+  className = "",
 }: ButtonProps) {
   return (
     <button
       type={type}
       onClick={onClick}
-      className="rounded bg-blue-600 px-4 py-2 text-white"
+      className={`rounded px-4 py-2 font-medium transition-all ${className}`}
     >
       {children}
     </button>
--- src/components/atomic/atoms/Input/Input.tsx
+++ src/components/atomic/atoms/Input/Input.tsx
@@ -1,5 +1,10 @@
 type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
-export default function Input(props: InputProps) {
-  return <input {...props} className="w-full rounded border px-3 py-2" />;
+export default function Input({ className, ...props }: InputProps) {
+  return (
+    <input
+      {...props}
+      className={`w-full rounded border px-3 py-2 outline-none transition-all focus:ring-2 focus:ring-indigo-400 ${className || ""}`}
+    />
+  );
 }
