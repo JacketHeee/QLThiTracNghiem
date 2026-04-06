ded732c00e368ba3118d43899d175fd8141fccd9
JacketHeee <3123560050@sv.sgu.edu.vn>
2026-04-06 10:54:45 +0700

    hotfix: finish fix error reload page

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,14 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
+  base: "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
+  },
+  server: {
+    port: 5173,
+    host: true,
+  },
 });
